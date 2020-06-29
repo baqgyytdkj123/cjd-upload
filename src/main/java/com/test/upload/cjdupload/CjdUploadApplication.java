c761be59e63284ebb117d68ced7efc5a690847b4
baqgyytdkj123 <baqgyytdkj@126.com>
2020-06-29 09:40:57 +0800

    上传测试接口

---
+++ src/main/java/com/test/upload/cjdupload/CjdUploadApplication.java
@@ -0,0 +1,13 @@
+package com.test.upload.cjdupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjdUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjdUploadApplication.class, args);
+    }
+
+}
